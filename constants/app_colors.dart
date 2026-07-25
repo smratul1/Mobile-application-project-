459f237a8c71ce9820183f1f21f2f2228cd4e142
smratul1 <124196475+smratul1@users.noreply.github.com>
2026-07-26 01:26:56 +0600

    Add files via upload

--- constants/app_colors.dart
+++ constants/app_colors.dart
@@ -6,6 +6,7 @@ class AppColors {
   static const Color primary = Color(0xFFC2185B);
   static const Color gradientStart = Color(0xFFE91E8C);
   static const Color gradientEnd = Color(0xFF7B1FA2);
+  static const Color accentPink = Color(0xFFE91E8C);
   static const Color background = Color(0xFFF8F9FF);
   static const Color card = Color(0xFFFFFFFF);
   static const Color text = Color(0xFF1A1A2E);
@@ -28,4 +29,15 @@ class AppColors {
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
   );
+
+  static const Color darkBackground = Color(0xFF0B0F19);
+  static const Color darkSurface = Color(0xFF111827);
+  static const Color darkCard = Color(0xFF1A1F2E);
+  static const Color darkCardAlt = Color(0xFF1E2536);
+  static const Color darkText = Color(0xFFE5E7EB);
+  static const Color darkTextSecondary = Color(0xFF9CA3AF);
+  static const Color darkBorder = Color(0xFF273141);
+  static const Color darkSecondary = Color(0xFF1F2937);
+  static const Color teal = Color(0xFF06B6D4);
+  static const Color tealDark = Color(0xFF0891B2);
 }
